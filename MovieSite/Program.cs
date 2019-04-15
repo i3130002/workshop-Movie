069872a28759058f5bc971a60c291bc6db9b27eb
I3130002-PC\i3130002 <i3130002@gmail.com>
2019-04-15 22:53:41 +0430

    Add project files.

---
+++ MovieSite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieSite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
